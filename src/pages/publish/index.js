5758f448a69f956e10da4684e61b3e8ce0152bc7
zengtianshengz <zengtianshengz@163.com>
2018-04-30 14:52:58 +0800

    nt

--- src/pages/publish/index.js
+++ src/pages/publish/index.js
@@ -6,7 +6,9 @@
 import React, {Component} from "react";
 import {connect} from 'react-redux';
 import ImagePicker from 'antd-mobile/lib/image-picker';  // 加载 JS
+import AV from 'leancloud-storage';
 import PropTypes from 'prop-types';
+import toast from 'src/components/toast'
 import {_add} from 'src/store/user/action'
 import 'antd-mobile/lib/image-picker/style/css';  // 加载 JS
 import './style.less'
@@ -18,14 +20,8 @@ class Pubilsh extends Component {
   }
   state = {
     msg: '',
-    commentMsg: '',
-    files: [{
-      url: 'https://zos.alipayobjects.com/rmsportal/PZUUCKTRIHWiZSY.jpeg',
-      id: '2121',
-    }, {
-      url: 'https://zos.alipayobjects.com/rmsportal/hqQWgTXdrlmVVYi.jpeg',
-      id: '2122',
-    }],
+    newsMsg: '',
+    files: [],
     multiple: false,
   }
   onChange = (files, type, index) => {
@@ -36,13 +32,52 @@ class Pubilsh extends Component {
   }
   handleTextarea(event) {
     let value = event.target.value;
-    this.setState({commentMsg: value})
+    this.setState({newsMsg: value})
+  }
+  async btnShareClick() {
+    const newsMsg = this.state.newsMsg
+    if (!newsMsg) {
+      return
+    }
+    const files = this.state.files
+    const listImg = []
+    try {
+      for (let i = 0, len = files.length; i < len; i++) {
+        let data = {base64: files[i].url};
+        let file = new AV.File(`PUBLISH_IMG_${new Date().getTime()}.png`, data);
+        const res = await file.save()
+        if (!res.url()) {
+          toast({msg: '网络请求出错'})
+          return
+        }
+        listImg.push(res.url())
+      }
+      const News = AV.Object.extend('News')
+      const news = new News();
+      const rd = parseInt((Math.random() * 10), 10);
+      news.set('userID', '1768244236' + rd);
+      news.set('nickName', '曾田生' + rd);
+      news.set('headImgUrl', 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png');
+      news.set('show', '1');
+      news.set('type', '1');
+      news.set('newsMsg', newsMsg);
+      news.set('listImg', listImg);
+      const resNews = await news.save()
+      if (resNews.id) {
+        toast({msg: '分享成功！'})
+      } else {
+        toast({msg: '网络请求出错'})
+      }
+    } catch (err) {
+      console.log(err)
+      toast({msg: '服务器错误，请稍后重试'})
+    }
   }
   render() {
-    const { files } = this.state;
+    const {files} = this.state;
     return (
       <section className="publish">
-        <textarea className='textarea' value={this.state.commentMsg} onChange={this.handleTextarea.bind(this)} rows="6" placeholder="方圆两公里，分享身边事"></textarea>
+        <textarea className='textarea' value={this.state.newsMsg} onChange={this.handleTextarea.bind(this)} rows="6" placeholder="方圆两公里，分享身边事"></textarea>
         <ImagePicker
           files={files}
           onChange={this.onChange}
@@ -50,7 +85,7 @@ class Pubilsh extends Component {
           selectable={files.length < 9}
           multiple={this.state.multiple}
         />
-        <div className="btn-submit">分享</div>
+        <div className="btn-submit" onClick={this.btnShareClick.bind(this)}>分享</div>
       </section>
     )
   }
